06968e1ecc30659215fca7991a85fe4c29ef940c
Dory <78720364+dooory@users.noreply.github.com>
2026-01-14 17:39:19 +1300

    Create folder system for tasks

--- src/index.js
+++ src/index.js
@@ -1,3 +1,52 @@
 import "./style.css";
 
-console.log("My Webpack template!");
+const FolderSystem = (() => {
+	let folders = [];
+
+	const getAllFolders = () => folders;
+	const getFolderById = (id) => folders.find((element) => element.id === id);
+	const getFolderIndex = (id) =>
+		folders.findIndex((element) => element.id === id);
+
+	const setFolderAsDefault = (folder) => (folder._default = true);
+
+	const newFolder = (title, index) => {
+		let folder = {
+			id: crypto.randomUUID(),
+			title,
+			tasks: {},
+			delete: () => {
+				deleteFolder(folder.id);
+			},
+			fakeIndex: index || folders.length,
+			_default: false,
+			setDefault: () => setFolderAsDefault(folder),
+		};
+
+		folders.push(folder);
+
+		return folder;
+	};
+
+	const deleteFolder = (id) => {
+		if (!getFolderById(id)) {
+			console.error(`No folder with id <${id}> found`);
+			return;
+		}
+
+		delete folders[getFolderIndex(id)];
+
+		return folders;
+	};
+
+	return {
+		newFolder,
+		getAllFolders,
+		getFolderById,
+	};
+})();
+
+const folder = FolderSystem.newFolder("Default");
+folder.setDefault();
+
+console.log(FolderSystem.getAllFolders());
