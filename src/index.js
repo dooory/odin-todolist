4c2e2b753a482b359ff658d327f0d63590277825
Dory <78720364+dooory@users.noreply.github.com>
2026-01-14 18:01:15 +1300

    add system for managing tasks

--- src/index.js
+++ src/index.js
@@ -51,10 +51,58 @@ const FolderSystem = (() => {
 		newFolder,
 		getAllFolders,
 		getFolderById,
+		getDefaultFolder,
+	};
+})();
+
+const TaskSystem = (() => {
+	let tasks = [];
+
+	const getAllTasks = () => tasks;
+	const getTaskById = (id) => tasks.find((element) => element.id === id);
+	const getTaskIndex = (id) =>
+		tasks.findIndex((element) => element.id === id);
+
+	const newTask = (title, folder, index) => {
+		let listPosition = index || tasks.length;
+
+		let task = {
+			id: crypto.randomUUID(),
+			title,
+			getListPosition: () => listPosition,
+			setListPosition: (position) => (listPosition = position),
+			delete: () => deleteTask(task.id),
+			folder: folder || FolderSystem.getDefaultFolder(),
+		};
+
+		tasks.push(task);
+
+		return task;
+	};
+
+	const deleteTask = (id) => {
+		if (!getTaskById(id)) {
+			console.error(`No folder with id <${id}> found`);
+			return;
+		}
+
+		delete tasks[getTaskIndex(id)];
+
+		return tasks;
+	};
+
+	return {
+		newTask,
+		getAllTasks,
+		getTaskById,
+		getTaskIndex,
 	};
 })();
 
 const folder = FolderSystem.newFolder("Default");
 folder.setDefault();
 
-console.log(FolderSystem.getAllFolders());
+const task = TaskSystem.newTask("Title");
+
+console.log(TaskSystem.getAllTasks());
+task.delete();
